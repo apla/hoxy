6ba85e3856d60d67161b09b1485c40a3e093a1f3
apla <dot.and.thing@gmail.com>
2013-11-06 04:58:39 +0300

    caching plugin

---
+++ plugins/cache-content.js
@@ -0,0 +1,73 @@
+/*
+Written by Greg Reimer
+Copyright (c) 2010
+http://github.com/greim
+*/
+
+/**
+Adds a banner to the top of an html page showing a message.
+usage: @banner('hey, this is a banner!')
+*/
+
+var crypto = require ('crypto');
+var fs     = require ('fs');
+var path   = require ('path');
+
+exports.run = function(api){
+	var res = api.getResponseInfo();
+	var req = api.getRequestInfo();
+
+	function cacheFileName (req) {
+		var shasum = crypto.createHash('sha1');
+		shasum.update(req.url);
+		var fileName = path.resolve ('cache', req.hostname + '-' + req.port + '-' + shasum.digest('hex'));
+		var ext = req.url.match (/\.[^\/]{1,10}$/);
+		return [fileName, ext];
+	}
+
+	if (res.statusCode == 200) {
+		
+		var fileName = cacheFileName (req);
+
+		var wm = fs.WriteStream (fileName[0]+'.meta');
+		wm.write (JSON.stringify ([res.statusCode, res.headers]));
+		wm.end ();
+
+		var w = fs.WriteStream (fileName[0]+'.data' + (fileName[1] ? '.'+fileName[1] : ''));
+		res.body.forEach (function (chunk){
+			w.write (chunk);
+		});
+		w.end();
+	} else if (res.statusCode == 206) {
+		// console.log (req.headers);
+		// console.log (res.headers);
+		var fileName = cacheFileName (req);
+
+		var wm = fs.WriteStream (fileName[0]+'.meta');
+		wm.write (JSON.stringify ([res.statusCode, res.headers]));
+		wm.end ();
+
+		var w = fs.WriteStream (fileName[0]+'.data' + (fileName[1] ? '.'+fileName[1] : ''));
+		res.body.forEach (function (chunk){
+			w.write (chunk);
+		});
+		w.end();
+
+	} else {
+		console.log (req.url + ' ignored due status code: ' + res.statusCode);
+
+	}
+// requestInfo.method
+// requestInfo.headers
+// requestInfo.url
+// requestInfo.hostname
+// requestInfo.port
+// requestInfo.body
+
+// responseInfo.headers
+// responseInfo.statusCode
+// responseInfo.body
+// responseInfo.throttle
+
+	api.notify();
+};
---
+++ plugins/cache-lookup.js
@@ -0,0 +1,86 @@
+/*
+Written by Greg Reimer
+Copyright (c) 2010
+http://github.com/greim
+*/
+
+/**
+Adds a banner to the top of an html page showing a message.
+usage: @banner('hey, this is a banner!')
+*/
+
+var crypto = require ('crypto');
+var fs     = require ('fs');
+var path   = require ('path');
+
+
+exports.run = function(api){
+	var res = api.getResponseInfo();
+	var req = api.getRequestInfo();
+
+	function cacheMiss (message) {
+		console.log ('CACHE MISS FOR: ' + req.host + ' ' + req.url + (message ? ' DUE: ' + message : ''));
+		api.notify();
+	}
+
+	function cacheFileName (req) {
+		var shasum = crypto.createHash('sha1');
+		shasum.update(req.url);
+		var fileName = path.resolve ('cache', req.hostname + '-' + req.port + '-' + shasum.digest('hex'));
+		var ext = req.url.match (/\.[^\/]{1,10}$/);
+		return [fileName, ext];
+	}
+
+	var fileName = cacheFileName (req);
+
+	var dataFileName = fileName[0]+'.data' + (fileName[1] ? '.'+fileName[1] : '');
+	var metaFileName = fileName[0]+'.meta';
+	fs.readFile (metaFileName, function (err, headerData) {
+		if (err) {
+			// ignore everything
+			cacheMiss('NO METAFILE');
+			return;
+		}
+		// console.log (stats);
+
+		var headers;
+		var statusCode;
+		try {
+			var headerDataJSON = JSON.parse (headerData);
+			statusCode = headerDataJSON[0];
+			headers    = headerDataJSON[1];
+		} catch (err) {
+			cacheMiss('CANNOT PARSE HEADERS');
+			return;
+		}
+
+		fs.readFile (dataFileName, function (err, fileData) {
+			if (err) {
+				cacheMiss('DATA FILE ERROR');
+				return;
+
+			}
+			console.log ('CACHE HIT!');
+			api.setResponseInfo ({
+				headers: headers,
+				statusCode: statusCode,
+				body: [fileData]
+			});
+			api.notify();	
+		});
+
+		
+	})
+
+// requestInfo.method
+// requestInfo.headers
+// requestInfo.url
+// requestInfo.hostname
+// requestInfo.port
+// requestInfo.body
+
+// responseInfo.headers
+// responseInfo.statusCode
+// responseInfo.body
+// responseInfo.throttle
+};
